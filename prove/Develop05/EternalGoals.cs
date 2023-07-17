407b3a0b4d1857d8cc5d6507f62794330a01a2c2
erickandreww <122584020+erickandreww@users.noreply.github.com>
2023-07-17 15:20:04 -0300

    Develop 5 - correcting english mistakes

--- prove/Develop05/EternalGoals.cs
+++ prove/Develop05/EternalGoals.cs
@@ -3,14 +3,14 @@ using System;
 public class EternalGoals : Goals
 {
     
-    // A contrutor to get the goal initial information
+    // A constructor to get the goal initial information
     public EternalGoals() : base()
     {
         _activityName = "EternalGoals";
         _goalStatus = false;
     }
 
-    // a construtor to work with tha save and load class
+    // a constructor to work with tha save and load class
     public EternalGoals(string activityName, string goalName, string description, 
     string pointsValue) : base(activityName, goalName, description, pointsValue)
     {
@@ -22,14 +22,14 @@ public class EternalGoals : Goals
         return $"{_activityName},{_goalName},{_description},{_pointsValue}";
     }
 
-    // a override method to mark goal complete and return the points
+    // an override method to mark the goal complete and return the points
     public override int Complete()
     {
         Console.WriteLine($"Congratulations! You have earned {_pointsValue} points!");
         return _pointsValue;
     }
 
-    // a override method to display the goal name, description, and if is completed or not
+    // an override method to display the goal name, description, and if is completed or not
     public override string Display()
     {
         return $"[ ] {_goalName} ({_description})";
